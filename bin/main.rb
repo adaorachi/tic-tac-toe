14a6c41fb40084b8383facc5d7ad5bf15330bad3
MaryAnn Chukwuka <annychuks07@gmail.com>
2019-10-28 18:01:42 +0100

    add player_name, player_marker methods

--- bin/main.rb
+++ bin/main.rb
@@ -1,64 +1,112 @@
-#!/usr/bin/env ruby
+# frozen_string_literal: true
 
-# Welcome Message
-puts "Welcome to Tic-Tac_toe!"
-
-game_on = true
-
-# Ask for player 1 name & marker
-puts "Enter your name: "
-name1 = gets.chomp
-
-# Ask for player 2 name & marker
-puts "Enter your name: "
-name2 = gets.chomp
-
-# Ask for user input any command for the game if exit then exit the program
-
-
-while true
-    current_player = name1
-    puts "Please #{current_player} pick a cell or type \"exit\" if you want exit the game"
-    command = gets.chomp
-    win_game, game_tie = false
-    break if command == "exit"
-    int_command = command.to_i
-    case int_command
-    when 1..9
-        puts "#{current_player} picked the #{command} cell"
-    else
-        puts "You typed a wrong command, please enter your command again"
-    end
-
-    # This displays a grid of 9 empty cells
-    puts "Display board"
-
-    #checks columns rows and diagonals if someone won the game or game is tied then set win_game or game_tie variable
-
-    if win_game
-        # Checks if a player has won
-        puts "Congrats! You have won!"
-        puts "Do you wanna to play again? (y/n)"
-        command = gets.chomp
-        if command == "n"
-            break
-        end
-
-        #reset_board
-    elsif game_tie
-        # Check if the game is tie
-        puts "Game Over. It is a tie!"
-        puts "Do you wanna to play again? (y/n)"
-        command = gets.chomp
-        if command == "n"
-            break
-        end
-
-        #reset_board
-    else
-        puts "Flip marker"
-        # current_player, other_player = other_player, current_player
-    end
+class Cell
+  def choose_position
+  end
 end
 
+class Player 
+  attr_accessor :name, :marker
+  def initialize(player = nil, name = nil)
+    @name = name
+    @player = player
+    @marker = marker
+  end
 
+  def player_name
+    puts "#{@player} Enter your name: "
+    name = gets.chomp
+
+    until name.is_a?(String) && name.length > 1
+      puts "Invalid Entry, name must be more than one character! Please re-enter your name: "
+      name = gets.chomp
+    end
+    @name = name
+
+  end
+
+  def random_player(*args)
+    args.shuffle
+  end
+
+  def player_marker
+    puts "#{@name.capitalize} choose your marker (X or O): "
+    marker = gets.chomp.upcase
+    until %w[X O].include? marker
+      puts "Invalid Entry #{@name.capitalize}, please re-enter your maker: "
+      marker = gets.chomp.upcase
+    end
+    @marker = marker
+  end
+end
+  
+class Board
+  def draw_board
+  end
+
+  def display_board
+  end
+
+  def winning_position
+  end
+
+  def win_row
+  end
+
+  def win_column
+  end
+
+  def win_diagonal
+  end
+
+  def draw
+  end
+end
+
+class Game  
+  def welcome_message
+    puts "Welcome to Tic Tac Toe \n\n"
+    puts "Press Enter to continue:"
+    gets.chomp
+
+  end
+
+  def game_over
+  end
+
+  def game_replay
+  end
+
+  def play
+    welcome_message
+
+    player1 = Player.new('Player 1')
+    player1.player_name
+
+    player2 = Player.new('Player 2')
+    player2.player_name
+
+    puts "\n\nHello #{player1.name.capitalize} and #{player2.name.capitalize}"
+
+    player_first = Player.new()
+    random = player_first.random_player(player1, player2)
+
+    player_first = random[0]
+    player_second = random[1]
+
+    puts "\n\n#{player_first.name.capitalize} goes first!"
+
+    player_first.player_marker
+
+    player_second.marker = player_first.marker == 'X' ? 'O' : 'X'
+
+    puts "\n\n"
+    puts "#{player_first.name.capitalize}, your marker is #{player_first.marker}"
+    puts "#{player_second.name.capitalize}, your marker is #{player_second.marker}"
+
+
+  end
+end
+
+play = Game.new
+play.play
