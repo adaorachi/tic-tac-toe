32bd29811253095a8b0a31edbd46a3f878f43f7f
MaryAnn Chukwuka <annychuks07@gmail.com>
2019-10-30 17:11:49 +0100

    add more functionalities

--- bin/main.rb
+++ bin/main.rb
@@ -55,18 +55,20 @@ class Board
   
   attr_accessor :cell_grid
   def initialize
+    @available = []
+    (0..9).each { @available << ' ' }
     reset
   end
 
   def draw
     row = cell_grid[0]
-    puts "#{row[0].content} | #{row[1].content} | #{row[2].content}"
-    puts "---------"
+    puts "#{row[0].content} | #{row[1].content} | #{row[2].content}       #{@available[0]} | #{@available[1]} | #{@available[2]}"
+    puts "---------       ---------"
     row = cell_grid[1]
-    puts "#{row[0].content} | #{row[1].content} | #{row[2].content}"
-    puts "---------"
+    puts "#{row[0].content} | #{row[1].content} | #{row[2].content}       #{@available[3]} | #{@available[4]} | #{@available[5]}"
+    puts "---------       ---------"
     row = cell_grid[2]
-    puts "#{row[0].content} | #{row[1].content} | #{row[2].content}"
+    puts "#{row[0].content} | #{row[1].content} | #{row[2].content}       #{@available[6]} | #{@available[7]} | #{@available[8]}"
   end
 
   def display_board
@@ -81,22 +83,22 @@ class Board
   end
 
   def win_row(player)
-      row1 = [@cell_grid[0][0], @cell_grid[0][1], @cell_grid[0][2]].all? {|x| x.marker == player.marker}
-      row2 = [@cell_grid[1][0], @cell_grid[1][1], @cell_grid[1][2]].all? {|x| x.marker == player.marker}
-      row3 = [@cell_grid[2][0], @cell_grid[2][1], @cell_grid[2][2]].all? {|x| x.marker == player.marker}
+      row1 = [@available[0], @available[1], @available[2]].all? {|x| x == player.marker}
+      row2 = [@available[3], @available[4], @available[5]].all? {|x| x == player.marker}
+      row3 = [@available[6], @available[7], @available[8]].all? {|x| x == player.marker}
       [row1, row2, row3].any?
   end
 
   def win_column(player)
-    col1 = [@cell_grid[0][0], @cell_grid[1][0], @cell_grid[2][0]].all? {|x| x.marker == player.marker}
-    col2 = [@cell_grid[0][1], @cell_grid[1][1], @cell_grid[2][1]].all? {|x| x.marker == player.marker}
-    col3 = [@cell_grid[0][2], @cell_grid[1][2], @cell_grid[2][2]].all? {|x| x.marker == player.marker}
+    col1 = [@available[0], @available[3], @available[6]].all? {|x| x == player.marker}
+    col2 = [@available[1], @available[4], @available[7]].all? {|x| x == player.marker}
+    col3 = [@available[2], @available[5], @available[8]].all? {|x| x == player.marker}
     [col1, col2, col3].any?
   end
 
   def win_diagonal(player)
-    dia1 = [@cell_grid[0][0], @cell_grid[1][1], @cell_grid[2][2]].all? {|x| x.marker == player.marker}
-    dia2 = [@cell_grid[0][2], @cell_grid[1][1], @cell_grid[2][0]].all? {|x| x.marker == player.marker}
+    dia1 = [@available[0], @available[4], @available[8]].all? {|x| x == player.marker}
+    dia2 = [@available[2], @available[4], @available[6]].all? {|x| x == player.marker}
     [dia1, dia2].any?
   end
 
@@ -118,9 +120,10 @@ class Board
     end
 
     if find_cell.marker == nil
-      find_cell.marker = player.marker
+      find_cell.marker = " "
+      @available[position-1] = player.marker
     else
-      puts "Cell already taken, please choose another position"
+      puts "Cell already taken #{player.name.capitalize}, please choose another position"
       choose_cell(player)
     end
     
@@ -148,10 +151,56 @@ class Game
     @game_on = true
   end
   def welcome_message
-    puts "Welcome to Tic Tac Toe \n\n"
-    puts "Press Enter to continue:"
+    sleep(1)
+    puts "\n\n"
+    puts "======================"
+    puts "WELCOME TO TIC TAC TOE"
+    puts "======================"
+    sleep(2)
+    puts "\n\nPress any key to continue:"
     gets.chomp
+    sleep(2)
+  end
 
+  def instruction
+    puts "\n"
+    puts "Press a to start the game: "
+    puts "Press b to see how to play: "
+    instruction = gets.chomp.downcase
+    while instruction == 'b'
+      sleep(2)
+      puts "\n\n"
+      puts "================================="
+      puts "You have chosen to see game rules"
+      puts "================================="
+      sleep(2)
+      puts "\n\n"
+      puts "Game Rules"
+      sleep(2)
+      puts "\n"
+      puts "1. The game is played on a grid that's 3 squares by 3 squares."
+      sleep(2)
+      puts "2. It is played by two persons and in which one of you will chosen at random to select a marker (X or O) and also start the game."
+      sleep(2)
+      puts "3. If you select X, the next player is O or vice-versa. Players take turns putting their marks in empty squares."
+      sleep(2)
+      puts "4. The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner."
+      sleep(2)
+      puts "5. When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie."
+      
+      sleep(2)
+      puts "\n\n"
+      puts "Press a to start the game: "
+      puts "Press b to see how to play: "
+      instruction = gets.chomp.downcase
+    end
+    if instruction == 'a'
+      sleep(2)
+      puts "\n\n"
+      puts "============================="
+      puts "You have chosen to start game"
+      puts "============================="
+    end
   end
 
   def game_over(player)
@@ -178,13 +227,19 @@ class Game
 
   def play
     welcome_message
+    instruction
 
     player1 = Player.new('Player 1')
+    sleep(2)
+    puts "\n\n"
     player1.player_name
 
     player2 = Player.new('Player 2')
+    sleep(2)
+    puts "\n\n"
     player2.player_name
 
+    sleep(2)
     puts "\n\nHello #{player1.name.capitalize} and #{player2.name.capitalize}"
 
     player_first = Player.new()
@@ -193,21 +248,36 @@ class Game
     player_first = random[0]
     player_second = random[1]
 
+    sleep(2)
     puts "\n\n#{player_first.name.capitalize} goes first!"
-
+  
+    sleep(2)
+    puts "\n\n"
     player_first.player_marker
 
     player_second.marker = player_first.marker == 'X' ? 'O' : 'X'
-
+    sleep(2)
     puts "\n\n"
     puts "#{player_first.name.capitalize}, your marker is #{player_first.marker}"
+    sleep(2)
     puts "#{player_second.name.capitalize}, your marker is #{player_second.marker}"
+    sleep(2)
+
+    puts "\n\n"
+    puts 'Displaying board...'
+    sleep(2)
+    puts "\n\n"
+    @board.draw
 
     player = player_first
-    @board.draw
     while @game_on
+      puts "\n\n"
       @board.choose_cell(player)
+      system('clear')
+      puts "\n\n"
       @board.draw
+      
+      puts "\n\n"
       game_over(player)
       player = player == player_first ? player_second : player_first
     end
