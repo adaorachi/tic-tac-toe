27605e162739a665926432a4e142303712b87344
MaryAnn Chukwuka <annychuks07@gmail.com>
2019-10-31 03:23:27 +0100

    fix a bug in instruction method

--- bin/main.rb
+++ bin/main.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+
 require_relative('../lib/player.rb')
 require_relative('../lib/cell.rb')
 require_relative('../lib/board.rb')
 
-class Game  
+class Game 
   attr_accessor :board
   def initialize
     @board = Board.new
@@ -27,9 +28,19 @@ class Game
   def game_over(player)
     if @board.winning_position(player)
       puts "Congratulations #{player.name.capitalize} wins!"
+      player.score += 1
+      sleep_mode(1)
+      puts 'Scores'
+      point1 = @player1.score > 1 ? 'points' : 'point'
+      point2 = @player2.score > 1 ? 'points' : 'point'
+
+      puts "#{@player1.name.capitalize} has #{@player1.score} #{point1}"
+      puts "#{@player2.name.capitalize} has #{@player2.score} #{point2}"
+
       @game_on = false
       game_replay(@player1, @player2)
     end
+
     game_tied = true
     @board.cell_grid.each do |row|
       if row.any? { |x| x.marker == nil }
@@ -39,7 +50,15 @@ class Game
     end
 
     if game_tied
-      puts "The game is a tie!."
+      puts 'The game is a tie!.'
+      sleep_mode(1)
+      puts 'Scores'
+      point1 = @player1.score > 1 ? 'points' : 'point'
+      point2 = @player2.score > 1 ? 'points' : 'point'
+
+      puts "#{@player1.name.capitalize} has #{@player1.score} #{point1}"
+      puts "#{@player2.name.capitalize} has #{@player2.score} #{point2}"
+
       @game_on = false
       game_replay(@player1, @player2)
     end
@@ -47,12 +66,12 @@ class Game
 
   def game_replay(player1, player2)
     sleep_mode(2)
-    puts "Do you want to play again (y/n)"
+    puts 'Do you want to play again (y/n)'
     replay = gets.chomp.downcase
     until %w[y n].include? replay
       sleep_mode(1)
-      puts "Invalid Entry!"
-      puts "Do you want to play again (y/n)"
+      puts 'Invalid Entry!'
+      puts 'Do you want to play again (y/n)'
       replay = gets.chomp.downcase
     end
     if replay == 'n'
@@ -63,20 +82,21 @@ class Game
       sleep_mode(3)
       puts "Alright then, replaying game for #{player1.name.capitalize} and #{player2.name.capitalize}!"
       sleep(1)
-      puts "..."
+      puts '...'
       sleep(1)
-      puts "..."
+      puts '...'
       sleep(1)
-      puts "..."
-  
+      puts '...'
+      sleep(1)
+
       system('clear')
-      puts "Displaying Board..."
+      puts 'Displaying Board...'
       sleep_mode(2)
 
       @board.reset
       draw_board(@board.cell_grid, @board.available)
       @game_on = true
-      game_flip      
+      game_flip
     end
   end
 
@@ -97,14 +117,14 @@ class Game
     marker = gets.chomp.upcase
     until %w[X O].include? marker
       sleep_mode(1)
-      puts "Invalid Entry #{player.name.capitalize}, please re-enter your maker: "
+      puts "Invalid Entry #{player.name.capitalize}, please re-enter your marker: "
       marker = gets.chomp.upcase
     end
     marker
   end
 
   def choose_cell(player)
-    puts "#{player.name.capitalize} choose your position, (1 - 9): "
+    puts "#{player.name.capitalize} (#{player.marker}), choose your position, (1 - 9): "
     position = gets.chomp.to_i
     until (1..9).include? position
       sleep_mode(1)
@@ -131,18 +151,18 @@ class Game
     end
   end
 
-  def sleep_mode(s)
-    sleep(s)
+  def sleep_mode(sec)
+    sleep(sec)
     puts "\n\n"
   end
 
   def welcome_message
     sleep_mode(2)
-    puts "======================"
-    puts "WELCOME TO TIC TAC TOE"
-    puts "======================"
+    puts '======================'
+    puts 'WELCOME TO TIC TAC TOE'
+    puts '======================'
     sleep_mode(2)
-    puts "Press any key to continue:"
+    puts 'Press any key to continue:'
     gets.chomp
     sleep_mode(2)
   end
@@ -150,42 +170,41 @@ class Game
   def instruction
     buttons = "Press a to start the game: \nPress b to see how to play: "
     puts buttons
-    instruction = gets.chomp.downcase
-    until %w[a b].include? instruction
+    instruct = gets.chomp.downcase
+    until %w[a b].include? instruct
       sleep_mode(1)
-      puts "Invalid Input!"
+      puts 'Invalid Input!'
+      sleep_mode(1)
       puts buttons
-      instruction = gets.chomp.downcase
+      instruct = gets.chomp.downcase
     end
-    while instruction == 'b'
+    if instruct == 'b'
       sleep_mode(2)
-      puts "================================="
-      puts "You have chosen to see game rules"
-      puts "================================="
+      puts '================================='
+      puts 'You have chosen to see game rules'
+      puts '================================='
 
       sleep_mode(2)
-      puts "Game Rules"
+      puts 'Game Rules'
 
       sleep_mode(2)
-      puts "1. The game is played on a grid that's 3 squares by 3 squares."
+      puts '1. The game is played on a grid that\'s 3 squares by 3 squares.'
       sleep(2)
-      puts "2. It is played by two persons and in which one of you will be chosen at random to select a marker (X or O) and also start the game."
+      puts '2. It is played by two persons and in which one of you will be chosen at random to select a marker (X or O) and also start the game.'
       sleep(2)
-      puts "3. If you select X, the next player is O or vice-versa. Players take turns putting their marks in empty squares."
+      puts '3. If you select X, the next player is O or vice-versa. Players take turns putting their marks in empty squares.'
       sleep(2)
-      puts "4. The first player to get 3 of his/her marks in a row (up, down, across, or diagonally) is the winner."
+      puts '4. The first player to get 3 of his/her marks in a row (up, down, across, or diagonally) is the winner.'
       sleep(2)
-      puts "5. When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie."
-      
+      puts '5. When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.'
+
       sleep_mode(2)
-      puts buttons
-      instruction = gets.chomp.downcase
-    end
-    if instruction == 'a'
+      instruction
+    elsif instruct == 'a'
       sleep_mode(2)
-      puts "============================="
-      puts "You have chosen to start game"
-      puts "============================="
+      puts '============================='
+      puts 'You have chosen to start game'
+      puts '============================='
     end
   end
 
@@ -205,7 +224,7 @@ class Game
     puts "Hello #{player1.name.capitalize} and #{player2.name.capitalize} :)"
   end
 
-  def game_flip  
+  def game_flip
     player_first = Player.new('')
     random = player_first.random_player(@player1, @player2)
 
@@ -214,7 +233,7 @@ class Game
 
     sleep_mode(2)
     puts "#{player_first.name.capitalize} goes first!"
-  
+
     sleep_mode(2)
     player_first.marker = player_marker(player_first)
 
@@ -230,20 +249,19 @@ class Game
 
     sleep_mode(2)
     draw_board(@board.cell_grid, @board.available)
+    sleep_mode(1)
 
     player = player_first
     while @game_on
-      sleep_mode(1)
       choose_cell(player)
+      sleep_mode(1)
       system('clear')
 
       draw_board(@board.cell_grid, @board.available)
       puts "\n\n"
       game_over(player)
       player = player == player_first ? player_second : player_first
-    end
-    
-   
+    end 
   end
 
   def play
--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Player
-  attr_accessor :marker
+  attr_accessor :marker, :score
   attr_reader :name
   def initialize(name)
     @name = name
     @marker = ''
+    @score = 0
   end
 
   def random_player(*args)
